6c50a533aac1c5b6701e52e3bf11d50700b12db1
Fran <=>
2024-03-29 17:03:05 +0100

    Landing page cover

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
+  const Image = `bg-[url('https://media.istockphoto.com/id/1305999733/es/foto/escritorio-de-dise%C3%B1o-web.jpg?s=1024x1024&w=is&k=20&c=G6lLvfLIFo6K3DJrzmKxFJHkGveQ3dC0QC8f1_pqhVY=')]`
   return (
     <main>
-      <section className=""> Landing page</section>
+      <section className={`${Image} absolute bg-cover bg-center  top-0 -z-10 h-screen  w-screen`}> 
+      
+      Landing page
+      
+      </section>
     </main>
   );
 }
