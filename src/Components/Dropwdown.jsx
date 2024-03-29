c57e494e7fb87df48326dac8591c164033d7fd20
Fran <=>
2024-03-29 18:03:27 +0100

    Dropdown styling

--- src/Components/Dropwdown.jsx
+++ src/Components/Dropwdown.jsx
@@ -6,11 +6,14 @@ export default function Dropdown({ HandleClick, setHandleClick, Links }) {
     <>
       {HandleClick && (
         <div className="absolute inset-0 bg-rose-600">
-          <ul className="flex flex-col h-full justify-center items-center ">
+          <ul className="flex flex-col h-full justify-center gap-4 items-center ">
             {Links.map(({ id, name, URL }) => (
-              <li className="p-7 font-semibold text-white" key={id}>
-                <Link onClick={() => setHandleClick(!HandleClick)} href={URL}>
-                  <span className="">{name}</span>
+              <li
+                className="relative w-44 h-12  font-medium text-white border-[1px]"
+                key={id}
+              >
+                <Link className="flex absolute inset-0 items-center justify-center" onClick={() => setHandleClick(!HandleClick)} href={URL}>
+                  {name}
                 </Link>
               </li>
             ))}
