6de9670718da028abb8493976cfe96234842d4fd
Fran <=>
2024-03-30 08:43:03 +0100

    Dropdown hidden on xl version

--- src/Components/Dropwdown.jsx
+++ src/Components/Dropwdown.jsx
@@ -5,7 +5,7 @@ export default function Dropdown({ HandleClick, setHandleClick, Links }) {
   return (
     <>
       {HandleClick && (
-        <div className="absolute inset-0 bg-rose-600">
+        <div className="absolute inset-0 bg-rose-600 xl:hidden">
           <ul className="flex flex-col h-full justify-center gap-4 items-center ">
             {Links.map(({ id, name, URL }) => (
               <li
