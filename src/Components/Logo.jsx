0ff90a36abffc096b0fbfd9334eea743f7e3072a
Fran <=>
2024-04-28 23:27:31 +0200

    Creación del logo!

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <span className='font-semibold text-2xl'>WEBTODAY!</span>
+  )
+}
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,6 +6,7 @@ import Links from "../Data/Links";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import Dropdown from "./Dropwdown";
+import Logo from "./Logo";
 
 export default function Navbar() {
   const [HandleClick, setHandleClick] = useState(false);
@@ -14,7 +15,7 @@ export default function Navbar() {
     <header className="bg-rose-600 shadow-sm shadow-rose-300">
       <nav className="mx-8 xl:m-auto max-w-7xl  flex justify-between items-center">
         <div className="text-white py-4 px-5">
-          <Link href={"/"}>Logo</Link>
+          <Link href={"/"}><Logo/></Link>
         </div>
         <div>
           <ul className="hidden xl:flex visible">
