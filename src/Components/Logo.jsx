0bf177ff0c6cdf0c1643141a0e4a2aa2b99db03f
Fran <=>
2024-04-29 00:05:12 +0200

    Creación del logo!

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <div className="font-semibold text-2xl">
       <span>WEB</span>
-      <span className="text-yellow-300">TODAY!</span>
+      <span className="">TODAY!</span>
     </div>
   );
 }
