1d9e73467de2fc85cd103e6ff5222d221e1aaeac
Fran <=>
2024-04-29 22:35:04 +0200

    isActive links

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <div className="font-semibold text-2xl">
       <span>WEB</span>
-      <span className="">TODAY!</span>
+      <span className="text-yellow-300">TODAY!</span>
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Links from "../Data/Links";
+import { usePathname } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import Dropdown from "./Dropwdown";
@@ -11,22 +12,30 @@ import Logo from "./Logo";
 export default function Navbar() {
   const [HandleClick, setHandleClick] = useState(false);
 
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
     <header className="bg-rose-600 shadow-sm shadow-rose-300">
       <nav className="mx-8 xl:m-auto max-w-7xl  flex justify-between items-center">
         <div className="text-white py-4 px-5">
-          <Link href={"/"}><Logo/></Link>
+          <Link href={"/"}>
+            <Logo />
+          </Link>
         </div>
         <div>
           <ul className="hidden xl:flex visible">
-            {Links.map(({ id, name, URL }) => (
-              <li
-                key={id}
-                className="py-4 px-5 cursor-pointer text-white hover:opacity-80 hover:transition-all delay-150"
-              >
-                <Link href={URL}>{name} </Link>
-              </li>
-            ))}
+            {Links.map(({ id, name, URL }) => {
+              const isActive = pathname.startsWith(URL);
+              return (
+                <li
+                  key={id}
+                  className="relative py-4 px-5 cursor-pointer text-white hover:opacity-80 hover:transition-all delay-150"
+                >
+                  <Link className={isActive ? "underline decoration-yellow-300 decoration-4 underline-offset-8" : ""} href={URL}>{name} </Link>
+                </li>
+              );
+            })}
           </ul>
           <div
             className="relative xl:hidden cursor-pointer p-3 overflow-hidden z-50"
