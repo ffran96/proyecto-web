fcaca091d4d658a1ba64ddf413f31aa746f42620
Fran <=>
2024-12-23 03:04:09 +0100

    Wip cartel

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   const BGImage = `bg-[url('https://media.istockphoto.com/id/1305999733/es/foto/escritorio-de-dise%C3%B1o-web.jpg?s=1024x1024&w=is&k=20&c=G6lLvfLIFo6K3DJrzmKxFJHkGveQ3dC0QC8f1_pqhVY=')]`;
   return (
     <main className="flex flex-col gap-6 justify-center items-center h-screen">
-      <h1 className="text-[#ff70f5] text-5xl font-bold">Webeo.es</h1>
+      <h1 className="text-[#ff70f5bc] text-5xl font-bold">Webeo.es</h1>
       {/*       <section
         className={`${BGImage} absolute bg-cover bg-center  bottom-0 -z-10 h-svh w-screen`}
       >
