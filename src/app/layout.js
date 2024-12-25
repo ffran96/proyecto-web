37e3e26f5dec655eb3c9758ddca6ef585e702abd
Fran <=>
2024-12-25 12:50:59 +0100

    Cambio de colores

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#000000f2]`}>
+      <body className={`${inter.className} bg-[#694848]`}>
 {/*         <Navbar />
  */}        {children}
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 export default function Home() {
   const BGImage = `bg-[url('https://media.istockphoto.com/id/1305999733/es/foto/escritorio-de-dise%C3%B1o-web.jpg?s=1024x1024&w=is&k=20&c=G6lLvfLIFo6K3DJrzmKxFJHkGveQ3dC0QC8f1_pqhVY=')]`;
   return (
-    <main className="flex flex-col gap-6 justify-center items-center h-screen">
-      <h1 className="text-[#ff70f5bc] text-5xl font-bold">Webeo.es</h1>
+    <main className="flex flex-col gap-6 mt-20 justify-start items-center">
+      <h1 className="text-[#FFB6DB] text-5xl font-bold">Webeo.es</h1>
       {/*       <section
         className={`${BGImage} absolute bg-cover bg-center  bottom-0 -z-10 h-svh w-screen`}
       >
         <article className="relative top-14"> Landing page </article>
       </section> */}
       <img src={"/wip.png"} alt="Work in progress" className="bg-cover" />
-      <p className="text-[#fce2faf5] text-2xl font-light italic">
+      <p className="text-[#FFB6DB] text-2xl font-light italic">
         We are working on it.
       </p>
     </main>
